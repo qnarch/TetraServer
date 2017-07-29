29b19f2f129d863426ed95e6d8a4108c802a78a3
Olof Sjödin <me@olofsjodin.se>
2017-07-29 13:26:24 +0200

    First commit

---
+++ Server.java
@@ -0,0 +1,23 @@
+package com.company;
+
+interface PowerUp {
+
+}
+
+class GameBoard {
+
+}
+
+class Player {
+
+}
+
+class Lobby {
+
+}
+
+public class Server {
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
